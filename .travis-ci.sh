fd8b1d1b3594fa9f3dcd625f3080132c74ccfbfb
Peter Newman <peterjnewman@gmail.com>
2015-01-16 00:05:59 +0000

    Fix config so we can actually configure, add initial Travis, although build still fails for me

---
+++ .travis-ci.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# This script is triggered from the script section of .travis.yml
+# It runs the appropriate commands depending on the task requested.
+
+CPP_LINT_URL="http://google-styleguide.googlecode.com/svn/trunk/cpplint/cpplint.py";
+
+COVERITY_SCAN_BUILD_URL="https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh"
+
+if [[ $TASK = 'lint' ]]; then
+  # run the lint tool only if it is the requested task
+  wget -O cpplint.py $CPP_LINT_URL;
+  chmod u+x cpplint.py;
+  ./cpplint.py $(find ./ -name "*.h" -or -name "*.cpp" | xargs)
+  if [[ $? -ne 0 ]]; then
+    exit 1;
+  fi;
+elif [[ $TASK = 'doxygen' ]]; then
+  # check doxygen only if it is the requested task
+  autoreconf -i && ./configure
+  # the following is a bit of a hack to build the files normally built during
+  # the build, so they are present for Doxygen to run against
+  #make builtfiles
+  # count the number of warnings
+  warnings=$(make doxygen-doc 2>&1 >/dev/null | wc -l)
+  if [[ $warnings -ne 0 ]]; then
+    # print the output for info
+    make doxygen-doc
+    echo "Found $warnings doxygen warnings"
+    exit 1;
+  else
+    echo "Found $warnings doxygen warnings"
+  fi;
+elif [[ $TASK = 'coverage' ]]; then
+  # Compile with coverage for coveralls
+  autoreconf -i && ./configure --enable-gcov && make && make check
+elif [[ $TASK = 'coverity' ]]; then
+  # Run Coverity Scan unless token is zero length
+  # The Coverity Scan script also relies on a number of other COVERITY_SCAN_
+  # variables set in .travis.yml
+  if [[ ${#COVERITY_SCAN_TOKEN} -ne 0 ]]; then
+    curl -s $COVERITY_SCAN_BUILD_URL | bash
+  else
+    echo "Skipping Coverity Scan as no token found, probably a Pull Request"
+  fi;
+else
+  # Otherwise compile and check as normal
+  autoreconf -i && ./configure && make distcheck
+fi
