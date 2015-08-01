a5d51e4ddf222d80ff282b7130c07164ed752b0b
Simon Newton <nomis52@gmail.com>
2015-08-01 11:18:44 -0700

    Add support for ACK_TIMER & queued messages to the proxy

--- firmware/src/proxy_model.c
+++ firmware/src/proxy_model.c
@@ -31,6 +31,7 @@
 // Various constants
 enum { NUMBER_OF_CHILDREN = 2 };
 enum { SOFTWARE_VERSION = 0x00000000 };
+static const uint16_t ACK_TIMER_DELAY = 1u;
 static const char DEFAULT_CHILD_DEVICE_LABEL[] = "Ja Rule Child Device";
 static const char CHILD_DEVICE_MODEL_DESCRIPTION[] =
     "Ja Rule Proxy Child Device";
@@ -47,6 +48,15 @@ static ChildDevice g_children[NUMBER_OF_CHILDREN];
 static const ResponderDefinition ROOT_RESPONDER_DEFINITION;
 static const ResponderDefinition CHILD_DEVICE_RESPONDER_DEFINITION;
 
+typedef struct {
+  uint8_t buffer[RDM_MAX_FRAME_SIZE];
+  unsigned int length;
+  unsigned int sub_device;
+} ProxyBuffer;
+
+static ProxyBuffer g_proxy_buffer;
+
+
 // Helper functions
 // ----------------------------------------------------------------------------
 static int HandleRequest(const RDMHeader *header, const uint8_t *param_data) {
@@ -59,6 +69,41 @@ static int HandleRequest(const RDMHeader *header, const uint8_t *param_data) {
   }
 
   return RDMResponder_DispatchPID(header, param_data);
+}
+
+static int HandleChildRequest(const RDMHeader *header,
+                              const uint8_t *param_data,
+                              unsigned int sub_device_index) {
+  if (RDMUtil_RequiresResponse(header->dest_uid) && g_proxy_buffer.length) {
+    return RDMResponder_BuildNack(header, NR_PROXY_BUFFER_FULL);
+  }
+  int response_size = HandleRequest(header, param_data);
+  if (response_size > 0 && header->command_class != DISCOVERY_COMMAND) {
+    memcpy(g_proxy_buffer.buffer, g_rdm_buffer, response_size);
+    g_proxy_buffer.length = response_size;
+    g_proxy_buffer.sub_device = sub_device_index;
+    response_size = RDMResponder_BuildAckTimer(header, ACK_TIMER_DELAY);
+    g_responder->queued_message_count = 1u;
+  }
+  return response_size;
+}
+
+static int RespondWithQueuedMessage(const RDMHeader *header) {
+  if (header->param_data_length != sizeof(uint8_t)) {
+    return RDMResponder_BuildNack(header, NR_FORMAT_ERROR);
+  }
+
+  RDMHeader *queued_header = (RDMHeader*) g_proxy_buffer.buffer;
+  RDMResponder_BuildHeader(header, queued_header->port_id,
+      queued_header->command_class, ntohs(queued_header->param_id),
+      queued_header->message_length);
+  memcpy(g_rdm_buffer + sizeof(RDMHeader),
+         g_proxy_buffer.buffer + sizeof(RDMHeader),
+         queued_header->message_length - sizeof(RDMHeader));
+  g_proxy_buffer.length = 0u;
+  g_children[g_proxy_buffer.sub_device].responder.queued_message_count = 0u;
+  g_proxy_buffer.sub_device = 0u;
+  return RDMUtil_AppendChecksum(g_rdm_buffer);
 }
 
 // Proxy PID Handlers
@@ -84,6 +129,12 @@ int ProxyModel_GetProxiedDevices(const RDMHeader *header,
 
 // Child PID Handlers
 // ----------------------------------------------------------------------------
+int ProxyModelChild_GetQueuedMessage(const RDMHeader *header,
+                                     UNUSED const uint8_t *param_data) {
+  RDMResponder_BuildHeader(header, ACK, GET_COMMAND_RESPONSE,
+                           PID_STATUS_MESSAGES, sizeof(RDMHeader));
+  return RDMUtil_AppendChecksum(g_rdm_buffer);
+}
 
 // Public Functions
 // ----------------------------------------------------------------------------
@@ -91,6 +142,8 @@ void ProxyModel_Initialize() {
   RDMResponder *temp = g_responder;
 
   // Initialize the proxy
+  g_proxy_buffer.length = 0u;
+  g_proxy_buffer.sub_device = 0;
 
   // Initialize the child devices.
   uint8_t parent_uid[UID_LENGTH];
@@ -130,16 +183,23 @@ static int ProxyModel_HandleRequest(const RDMHeader *header,
     }
   }
 
+  // If there is message buffered, see if we need to return it
+  if (header->command_class == GET_COMMAND &&
+      ntohs(header->param_id) == PID_QUEUED_MESSAGE &&
+      g_proxy_buffer.length &&
+      RDMUtil_UIDCompare(header->dest_uid, ((RDMHeader*)
+                         g_proxy_buffer.buffer)->src_uid) == 0) {
+    return RespondWithQueuedMessage(header);
+  }
+
+  RDMResponder *temp = g_responder;
   unsigned int i = 0u;
   for (; i < NUMBER_OF_CHILDREN; i++) {
     if (RDMUtil_RequiresAction(g_children[i].responder.uid, header->dest_uid)) {
-      RDMResponder *temp = g_responder;
       g_responder = &g_children[i].responder;
-      response_size = HandleRequest(header, param_data);
+      int response_size = HandleChildRequest(header, param_data, i);
       g_responder = temp;
       if (response_size) {
-        // TODO(simon): make this more interesting by using ACK-TIMER in the
-        // responses.
         return response_size;
       }
     }
@@ -209,18 +269,20 @@ static const ResponderDefinition ROOT_RESPONDER_DEFINITION = {
 // ----------------------------------------------------------------------------
 
 static const PIDDescriptor CHILD_DEVICE_PID_DESCRIPTORS[] = {
-  {PID_SUPPORTED_PARAMETERS, RDMResponder_GetSupportedParameters, 0,
+  {PID_QUEUED_MESSAGE, ProxyModelChild_GetQueuedMessage, 1u,
     (PIDCommandHandler) NULL},
-  {PID_DEVICE_INFO, RDMResponder_GetDeviceInfo, 0, (PIDCommandHandler) NULL},
-  {PID_PRODUCT_DETAIL_ID_LIST, RDMResponder_GetProductDetailIds, 0,
+  {PID_SUPPORTED_PARAMETERS, RDMResponder_GetSupportedParameters, 0u,
     (PIDCommandHandler) NULL},
-  {PID_DEVICE_MODEL_DESCRIPTION, RDMResponder_GetDeviceModelDescription, 0,
+  {PID_DEVICE_INFO, RDMResponder_GetDeviceInfo, 0u, (PIDCommandHandler) NULL},
+  {PID_PRODUCT_DETAIL_ID_LIST, RDMResponder_GetProductDetailIds, 0u,
     (PIDCommandHandler) NULL},
-  {PID_MANUFACTURER_LABEL, RDMResponder_GetManufacturerLabel, 0,
+  {PID_DEVICE_MODEL_DESCRIPTION, RDMResponder_GetDeviceModelDescription, 0u,
     (PIDCommandHandler) NULL},
-  {PID_SOFTWARE_VERSION_LABEL, RDMResponder_GetSoftwareVersionLabel, 0,
+  {PID_MANUFACTURER_LABEL, RDMResponder_GetManufacturerLabel, 0u,
     (PIDCommandHandler) NULL},
-  {PID_IDENTIFY_DEVICE, RDMResponder_GetIdentifyDevice, 0,
+  {PID_SOFTWARE_VERSION_LABEL, RDMResponder_GetSoftwareVersionLabel, 0u,
+    (PIDCommandHandler) NULL},
+  {PID_IDENTIFY_DEVICE, RDMResponder_GetIdentifyDevice, 0u,
     RDMResponder_SetIdentifyDevice},
 };
 
