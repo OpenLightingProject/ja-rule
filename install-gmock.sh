f7b014609777d56e0a4063e6d9113e1bf7fba239
Simon Newton <nomis52@gmail.com>
2015-09-17 10:03:38 -0700

    Switch to the new gmock

--- install-gmock.sh
+++ install-gmock.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -ex
-svn checkout http://googlemock.googlecode.com/svn/trunk/ googlemock-read-only
-cd googlemock-read-only
+
+GTEST_DIRECTORY=gtest-read-only
+
+#svn checkout http://googlemock.googlecode.com/svn/trunk/ googlemock-read-only
+git clone git@github.com:google/googletest.git $GTEST_DIRECTORY
+cd $GTEST_DIRECTORY/googlemock
 autoreconf -fi
 ./configure
 make
-cd ..
