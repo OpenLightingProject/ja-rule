0e73679a631055030b23d2ea4e72c63c90ffc5cd
Simon Newton <nomis52@gmail.com>
2015-09-17 19:17:36 -0700

    Use the anonymous clone

--- install-gmock.sh
+++ install-gmock.sh
@@ -2,9 +2,9 @@
 set -ex
 
 GTEST_DIRECTORY=gtest-read-only
+GTEST_REPO=https://github.com/google/googletest.git
 
-#svn checkout http://googlemock.googlecode.com/svn/trunk/ googlemock-read-only
-git clone git@github.com:google/googletest.git $GTEST_DIRECTORY
+git clone $GTEST_REPO $GTEST_DIRECTORY
 cd $GTEST_DIRECTORY/googlemock
 autoreconf -fi
 ./configure
