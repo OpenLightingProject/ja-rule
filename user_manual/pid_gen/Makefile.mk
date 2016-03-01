3516ec4698d4e324e75594d6dd5b9e48d406347e
Simon Newton <nomis52@gmail.com>
2016-03-01 08:48:57 -0800

    Fix the build (again)

--- user_manual/pid_gen/Makefile.mk
+++ user_manual/pid_gen/Makefile.mk
@@ -3,18 +3,21 @@
 noinst_PROGRAMS += user_manual/pid_gen/pid_gen
 
 user_manual_pid_gen_pid_gen_SOURCES = user_manual/pid_gen/pid_gen.cpp
-user_manual_pid_gen_pid_gen_CXXFLAGS = $(TESTING_CXXFLAGS) $(OLA_CFLAGS)
-user_manual_pid_gen_pid_gen_LDADD = $(TESTING_LIBS) $(OLA_LIBS) \
-                                firmware/src/libdimmermodel.la \
-                                firmware/src/libledmodel.la \
-                                firmware/src/libmovinglightmodel.la \
-                                firmware/src/libnetworkmodel.la \
-                                firmware/src/libproxymodel.la \
-                                firmware/src/librandom.la \
-                                firmware/src/librdmbuffer.la \
-                                firmware/src/librdmresponder.la \
-                                firmware/src/librdmutil.la \
-                                firmware/src/libreceivercounters.la \
-                                firmware/src/libsensormodel.la \
-                                firmware/src/libcoarsetimer.la \
-                                tests/harmony/mocks/libharmonymock.la
+user_manual_pid_gen_pid_gen_CXXFLAGS = $(BUILD_FLAGS)  -I tests/include \
+                                       $(GMOCK_INCLUDES) $(GTEST_INCLUDES) \
+                                       $(OLA_CFLAGS)
+user_manual_pid_gen_pid_gen_LDADD = $(OLA_LIBS) \
+                                    firmware/src/libdimmermodel.la \
+                                    firmware/src/libledmodel.la \
+                                    firmware/src/libmovinglightmodel.la \
+                                    firmware/src/libnetworkmodel.la \
+                                    firmware/src/libproxymodel.la \
+                                    firmware/src/librandom.la \
+                                    firmware/src/librdmbuffer.la \
+                                    firmware/src/librdmresponder.la \
+                                    firmware/src/librdmutil.la \
+                                    firmware/src/libreceivercounters.la \
+                                    firmware/src/libsensormodel.la \
+                                    firmware/src/libcoarsetimer.la \
+                                    tests/harmony/mocks/libharmonymock.la \
+                                    $(GMOCK_LIBS) $(GTEST_LIBS)
