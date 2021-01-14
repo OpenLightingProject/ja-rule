7d5f53248e6f75948184c29b69af0ca01b02b805
Peter Newman <peternewman@users.noreply.github.com>
2021-01-14 01:17:25 +0000

    Fix the Travis build

--- install-gmock.sh
+++ install-gmock.sh
@@ -11,9 +11,7 @@ else
   git clone $GTEST_REPO $GTEST_DIRECTORY
   cd $GTEST_DIRECTORY;
 fi
-# Ideally do some clever detection of which flavour of gmock people have, for
-# now they'll just need to upgrade...
-#cd googlemock
-autoreconf -fi
-./configure
-make
+cd googlemock
+mkdir build
+cd build
+cmake ..
